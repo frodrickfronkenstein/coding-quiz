9750bb532cc5098dfa3433e0a3ee3b9d830370b0
frodrickfronkenstein <ericandrewsmith88@gmail.com>
2022-05-15 18:12:40 -0400

    first question displaying

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,18 @@
 // constant DOMs
 var timerEl = document.getElementById('timer');
 
-// Landing page DOM
+// Landing page DOMs
 var landingPageEl = document.querySelector("#landing-page");
 var startButtonEl = document.querySelector("#start-quiz");
+
+// Quiz DOMs
+var quizEl = document.querySelector("#quiz");
+var questionEl = document.querySelector("#question");
+var answer1El = document.querySelector("#answer1");
+var answer2El = document.querySelector("#answer2");
+var answer3El = document.querySelector("#answer3");
+var answer4El = document.querySelector("#answer4");
+document.querySelector("#quiz").style.display = "none";
 
 // questions array
 var questionsArray = [
@@ -49,7 +58,6 @@ var questionsArray = [
     }
 ];
 
-
 // basic timer function
 function quizTimer() {
     var beginTimer = 75;
@@ -66,9 +74,26 @@ function quizTimer() {
     }, 1000);
 }
 
+// starts quiz with first question
+var questionNumber = 0;
+
+var createQuestions = function() {
+    questionEl.innerHTML = questionsArray[questionNumber].question;
+    answer1El.innerHTML = questionsArray[questionNumber].answers.answerOne;
+    answer2El.innerHTML = questionsArray[questionNumber].answers.answerTwo;
+    answer3El.innerHTML = questionsArray[questionNumber].answers.answerThree;
+    answer4El.innerHTML = questionsArray[questionNumber].answers.answerFour;
+};
+
+var cycleQuestions = function() {
+    
+}
+
 // start quiz
 startButtonEl.addEventListener("click", function() {
     console.log("button clicked!");
     landingPageEl.style.display = "none";
+    quizEl.style.display = "block";
+    createQuestions();
     quizTimer();
 });
