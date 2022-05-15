9f48c05bc639fafec41e5213718330d38d4f18ec
frodrickfronkenstein <ericandrewsmith88@gmail.com>
2022-05-15 17:00:46 -0400

    landing-page functional

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,13 @@
+// constant DOMs
 var timerEl = document.getElementById('timer');
 
+// Landing page DOM
+var landingPageEl = document.querySelector("#landing-page");
+var startButtonEl = document.querySelector("#start-quiz");
 
-function timer() {
+
+// basic timer function
+function quizTimer() {
     var beginTimer = 75;
 
     var timeInterval = setInterval(function () {
@@ -15,3 +21,10 @@ function timer() {
         }
     }, 1000);
 }
+
+// start quiz
+startButtonEl.addEventListener("click", function() {
+    console.log("button clicked!");
+    landingPageEl.style.display = "none";
+    quizTimer();
+});
