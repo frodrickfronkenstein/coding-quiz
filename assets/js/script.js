76aabb43dd04bdb201d1556b51d0e2140e3a5298
frodrickfronkenstein <ericandrewsmith88@gmail.com>
2022-05-15 16:41:52 -0400

    timer function fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,17 @@
 var timerEl = document.getElementById('timer');
-var beginTimer = 75;
 
-var timeInterval = setInterval(function () {
-    if (beginTimer > 0) {
-        timerEl.textContent = "timer: " + beginTimer;
-        beginTimer--;
-    }else if (beginTimer === 0) {
-        timerEl.textContent = "OUT OF TIME!";
-    } else {
-        clearInterval(timeInterval);
-    }
-}, 1000);
-timeInterval();
+
+function timer() {
+    var beginTimer = 75;
+
+    var timeInterval = setInterval(function () {
+        if (beginTimer > 0) {
+            timerEl.textContent = "timer: " + beginTimer;
+            beginTimer--;
+        }else if (beginTimer === 0) {
+            timerEl.textContent = "OUT OF TIME!";
+        } else {
+            clearInterval(timeInterval);
+        }
+    }, 1000);
+}
