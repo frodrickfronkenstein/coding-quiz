8676ffac8188587dd3e6b10514abc17382d20f3a
frodrickfronkenstein <ericandrewsmith88@gmail.com>
2022-05-15 16:33:48 -0400

    timer added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
-buttonEl.addEventListener("click", createTaskHandler);
+var timerEl = document.getElementById('timer');
+var beginTimer = 75;
+
+var timeInterval = setInterval(function () {
+    if (beginTimer > 0) {
+        timerEl.textContent = "timer: " + beginTimer;
+        beginTimer--;
+    }else if (beginTimer === 0) {
+        timerEl.textContent = "OUT OF TIME!";
+    } else {
+        clearInterval(timeInterval);
+    }
+}, 1000);
+timeInterval();
